634d6fa44dec028cbb074bb10ba0e267e43eb5fb
benjiyamin <benjiyamin@gmail.com>
2015-08-19 00:51:47 -0400

    Added login and favoriting features

--- noraebook/urls.py
+++ noraebook/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
-from noraebook import settings
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
