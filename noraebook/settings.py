f137d81f214afc2d5d19a84ef7d8ee0c42b2da1b
benjiyamin <benjiyamin@gmail.com>
2015-08-19 14:20:47 -0400

    python anywhere friendly

--- noraebook/settings.py
+++ noraebook/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import mysql.connector.django
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,12 +82,14 @@ WSGI_APPLICATION = 'noraebook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Internationalization
--- noraebook/settings_production.py
+++ noraebook/settings_production.py
@@ -7,3 +7,14 @@ TEMPLATE_DEBUG = DEBUG
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'benjiyamin$noraebook',
+        'USER': 'benjiyamin',
+        'PASSWORD': 'ch1ck3nb0wl',
+        'HOST': 'benjiyamin.mysql.pythonanywhere-services.com',
+    }
+}
