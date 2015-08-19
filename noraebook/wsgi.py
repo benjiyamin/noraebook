232163234f289fa2187229b4b6120398c32327dd
benjiyamin <benjiyamin@gmail.com>
2015-08-19 01:01:28 -0400

    Added login and favoriting features

--- noraebook/wsgi.py
+++ noraebook/wsgi.py
@@ -20,8 +20,7 @@ application = get_wsgi_application()
 # HEROKU
 
 from dj_static import Cling
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noraebook.settings_production")
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = Cling(get_wsgi_application())
