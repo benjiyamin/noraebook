bc51ec71158367ca0cd39ae9b8777a152e9a7273
benjiyamin <benjiyamin@gmail.com>
2015-08-19 01:15:14 -0400

    Added login and favoriting features

--- noraebook/settings_production.py
+++ noraebook/settings_production.py
@@ -6,7 +6,7 @@ from .settings import *
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
