0c93cc8f24ba3541ae2289543be055d66908d2be
benjiyamin <benjiyamin@gmail.com>
2015-08-19 00:16:04 -0400

    Added login and favoriting features

--- noraebook/wsgi.py
+++ noraebook/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noraebook.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "noraebook.settings")
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
 
 
