a57898a2e2e6997078c355509930762c35f0b7a0
benjiyamin <benjiyamin@gmail.com>
2015-08-19 00:29:51 -0400

    Added login and favoriting features

--- noraebook/settings_production.py
+++ noraebook/settings_production.py
@@ -17,7 +17,8 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = "noraebook.herokuapp.com"
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
