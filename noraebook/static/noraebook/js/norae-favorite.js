62ef7ae4b8d2cc6e6a60de062edddc41420602d1
benjiyamin <benjiyamin@gmail.com>
2015-08-19 11:51:08 -0400

    python anywhere friendly

--- noraebook/static/noraebook/js/norae-favorite.js
+++ noraebook/static/noraebook/js/norae-favorite.js
@@ -1,19 +1,17 @@
-$(document).ready(function() {
-    $(".likable").click(function(event) {
-        event.preventDefault();
-        if ($(this).hasClass("not-liked")) {
-            $(this).removeClass("not-liked");
-            $(this).addClass("liked");
-        } else {
-            $(this).removeClass("liked");
-            $(this).addClass("not-liked");
+$(".likable").click(function(event) {
+    event.preventDefault();
+    if ($(this).hasClass("not-liked")) {
+        $(this).removeClass("not-liked");
+        $(this).addClass("liked");
+    } else {
+        $(this).removeClass("liked");
+        $(this).addClass("not-liked");
+    }
+    $.ajax({
+        type: 'POST',
+        url: "/like/",
+        data: {
+            'song_id': event.target.id
         }
-        $.ajax({
-            type: 'POST',
-            url: "/like/",
-            data: {
-                'song_id': event.target.id
-            }
-        });
     });
 });
