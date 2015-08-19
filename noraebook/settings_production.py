782935f60cd2d525f047e98a71df4f49fc8b87ca
benjiyamin <benjiyamin@gmail.com>
2015-08-19 14:36:53 -0400

    python anywhere friendly

--- noraebook/settings_production.py
+++ noraebook/settings_production.py
@@ -8,13 +8,3 @@ TEMPLATE_DEBUG = DEBUG
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'benjiyamin$noraebook',
-        'USER': 'benjiyamin',
-        'PASSWORD': 'ch1ck3nb0wl',
-        'HOST': 'benjiyamin.mysql.pythonanywhere-services.com',
-    }
-}
