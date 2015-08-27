2fb320ade6e89f1b87507c998f2651ef47aea984
Ben Miller <benjiyamin@gmail.com>
2015-08-27 07:48:58 -0400

    git updates

--- noraebook/settings_production.py
+++ noraebook/settings_production.py
@@ -1,6 +1,17 @@
 from .settings import *
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'benjiyamin$noraebook',
+        'USER': 'benjiyamin',
+        'PASSWORD': 'ch1ck3nb0wl',
+        'HOST': 'mysql.server',
+    }
+}
+
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
